c84da9242e27789f5ae43ba508608732343b6db7
Gabriel Mičko <micsko@gmail.com>
2017-07-13 21:39:41 -0300

    Cleanup

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// app.get('/', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/examples/speakers.html'));
-//   //__dirname : It will resolve to your project folder.
-// });
-
 app.get('/speakers', function(req, res) {
   res.sendFile(path.join(__dirname + '/examples/speakers.html'));
 });
@@ -22,10 +17,6 @@ app.get('/speakers_min', function(req, res) {
 app.get('/speakers_with_talks', function(req, res) {
   res.sendFile(path.join(__dirname + '/examples/speakers_with_talks.html'));
 });
-
-// app.get('/sitemap', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/sitemap.html'));
-// });
 
 app.listen(3001);
 
